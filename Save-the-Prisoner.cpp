8d8830d1a222bedfb115a6d4a2b9567667897049
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-05-09 02:16:12 +0900

    Create Save-the-Prisoner.cpp

---
+++ Save-the-Prisoner.cpp
@@ -0,0 +1,16 @@
+int saveThePrisoner(int n, int m, int s)
+{
+    int result = ((m%n) +s -1) % n;
+    return result ? result : n;
+}
+
+int saveThePrisoner1(int n, int m, int s)
+{
+    int result = (m+s-1)%n;
+    return result ? result : n;
+}
+
+int saveThePrisoner2(int n, int m, int s)
+{
+    return ((m+s-2) % n) + 1;
+}
